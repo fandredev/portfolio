b23b98bef57919f8d4289a28590b5ecfd9558ccd
felipe <profissionalf.andre@gmail.com>
2024-09-13 19:56:43 -0300

    feat: Add Social component for displaying social icons
    This commit adds a new component called Social, which is responsible for displaying social icons on the home page. The component includes links to the developer's GitHub and LinkedIn profiles. This feature enhances the user experience by providing easy access to the developer's social media presence.

---
+++ src/components/social/index.tsx
@@ -0,0 +1,20 @@
+export default function Social() {
+  return (
+    <div className="home__social">
+      <a
+        href="https://github.com/fandredev/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-github-alt"></i>
+      </a>
+      <a
+        href="https://github.com/fandredev/"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <i className="uil uil-linkedin"></i>
+      </a>
+    </div>
+  );
+}
