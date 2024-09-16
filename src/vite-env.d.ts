e27a80c199f16c6d54f87a3d554082ff77eb719d
felipe <profissionalf.andre@gmail.com>
2024-09-16 19:23:13 -0300

    refactor: Update environment variables for email service

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,11 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_SERVICE_EMAIL_ID: string;
+  readonly VITE_TEMPLATE_EMAIL_ID: string;
+  readonly VITE_EMAIL_API_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
