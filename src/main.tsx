73eae829b7af6988ac94f5e4a595211156c84a09
felipe <profissionalf.andre@gmail.com>
2024-09-16 19:26:00 -0300

    refactor: Add ToastContainer for notifications and import ReactToastify CSS
    - Added ToastContainer component from 'react-toastify' library to display notifications.
    - Imported 'ReactToastify.css' to apply styles to the notifications.

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import Portfolio from './App.tsx';
 
 import './i18n';
+import 'react-toastify/dist/ReactToastify.css';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Portfolio />
