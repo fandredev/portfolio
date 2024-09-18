8aed73e9aa32be79fd21df8490f8210ab456fbeb
felipe <profissionalf.andre@gmail.com>
2024-09-18 02:37:01 -0300

    refactor: update Portuguese translation in home page
    - Update Portuguese translation in the home page to improve readability and consistency.

--- src/i18n/locales/pt-br.ts
+++ src/i18n/locales/pt-br.ts
@@ -1,85 +1,81 @@
 export default {
   translations: {
     home: {
-      beginning: 'Ínicio',
-      about: 'Sobre',
-      skills: 'Skills',
-      qualifications: 'Qualificações',
-      contacts: 'Contatos',
+      beginning: "Ínicio",
+      about: "Sobre",
+      skills: "Skills",
+      qualifications: "Qualificações",
+      contacts: "Contatos",
       introduction_text:
-        'Olá! Meu nome é Felipe André, sou um desenvolvedor Full Stack. Sou apaixonado por tecnologia e estou sempre em busca de novos desafios.',
-      say_hello_whatsapp_button: 'Diga oi',
-      scroll_down: 'Role para baixo',
-      about_me_title: 'Sobre mim',
-      an_basic_introduction: 'Uma breve introdução',
+        "Olá! Meu nome é Felipe André, sou um desenvolvedor Full Stack. Sou apaixonado por tecnologia e estou sempre em busca de novos desafios.",
+      say_hello_whatsapp_button: "Diga oi",
+      scroll_down: "Role para baixo",
+      about_me_title: "Sobre mim",
+      an_basic_introduction: "Uma breve introdução",
       description_text:
-<<<<<<< HEAD
-        'Meu nome é Felipe André e tenho 24 anos e moro na região oeste de São Paulo. Sou estudante de tecnologia desde os 17 anos e sou grato pelo progresso que venho aprendendo com a web e com esse mundo vasto de ser uma pessoa programadora. Atualmente estou trabalhando como Programador Pleno em uma startup de gestão de contratos e processos para advogados e hoje trabalho com as tecnologias de Django usando Python e React + Typescript.',
-=======
-        'Meu nome é Felipe André, adoro astronomia e moro na região oeste de São Paulo, Brasil. Sou estudante de tecnologia desde os 17 anos e sou grato pelo progresso que venho aprendendo com a web e com esse mundo vasto de ser uma pessoa programadora. Atualmente estou trabalhando como Programador Pleno em uma startup de gestão de contratos e processos para advogados e hoje trabalho com as tecnologias de Django usando Python e React + Typescript.',
->>>>>>> c8ba28b (refactor: update .editorconfig and footer CSS)
-      download_curriculum: 'Baixar currículo',
-      years_as_developer: '4 anos como desenvolvedor',
-      quantity_projects_completed: '+16 projetos concluídos',
-      how_i_work: 'Remoto e presencial',
-      skills_subtitle: 'Meu nível técnico em programação',
-      loading: 'Carregando...',
+        "Meu nome é Felipe André, adoro astronomia e moro na região oeste de São Paulo, Brasil. Sou estudante de tecnologia desde os 17 anos e sou grato pelo progresso que venho aprendendo com a web e com esse mundo vasto de ser uma pessoa programadora. Atualmente estou trabalhando como Programador Pleno em uma startup de gestão de contratos e processos para advogados e hoje trabalho com as tecnologias de Django usando Python e React + Typescript.",
+      download_curriculum: "Baixar currículo",
+      years_as_developer: "4 anos como desenvolvedor",
+      quantity_projects_completed: "+16 projetos concluídos",
+      how_i_work: "Remoto e presencial",
+      skills_subtitle: "Meu nível técnico em programação",
+      loading: "Carregando...",
     },
     tests: {
-      title: 'Testes',
+      title: "Testes",
     },
     tools: {
-      tools_day_by_day: 'Ferramentas do dia a dia',
-      four_years_of_use: '4 anos de uso',
-      three_years_of_use: '3 anos de uso',
-      two_years_of_use: '2 anos de uso',
-      one_year_of_use: '1 ano de uso',
-      seven_months_of_use: '7 meses',
+      tools_day_by_day: "Ferramentas do dia a dia",
+      four_years_of_use: "4 anos de uso",
+      three_years_of_use: "3 anos de uso",
+      two_years_of_use: "2 anos de uso",
+      one_year_of_use: "1 ano de uso",
+      seven_months_of_use: "7 meses",
     },
     portfolio: {
       my_projects_personally:
-        'Alguns dos meus projetos pessoais. Sempre confira meu GitHub para mais projetos.',
+        "Alguns dos meus projetos pessoais. Sempre confira meu GitHub para mais projetos.",
     },
     education: {
       assistente_de_desenvolvimento_de_aplicativos_computacionais:
-        'Assistente de desenvolvimento de aplicativos computacionais',
+        "Assistente de desenvolvimento de aplicativos computacionais",
       assistente_de_operacao_de_redes_de_computadores:
-        'Assistente de operação de redes de computadores',
+        "Assistente de operação de redes de computadores",
     },
     experiences: {
-      junior_developer: 'Programador Júnior',
-      pleno_full_stack_developer: 'Desenvolvedor Full-Stack Pleno',
-      actual: 'Atual',
+      junior_developer: "Programador Júnior",
+      pleno_full_stack_developer: "Desenvolvedor Full-Stack Pleno",
+      actual: "Atual",
     },
     qualification: {
-      title: 'Qualificação',
-      subtitle: 'Minha jornada profissional',
+      title: "Qualificação",
+      subtitle: "Minha jornada profissional",
     },
     tabs: {
-      experience: 'Experiência',
-      education: 'Educação',
+      experience: "Experiência",
+      education: "Educação",
     },
 
     contact: {
-      send_email: 'Mande um email',
-      send_message: 'Enviar mensagem',
-      title: 'Entre em contato',
-      subtitle: 'Contate-me',
-      send: 'Enviar',
+      send_email: "Mande um email",
+      send_message: "Enviar mensagem",
+      title: "Entre em contato",
+      subtitle: "Contate-me",
+      send: "Enviar",
 
       form: {
-        fill_name: 'Por favor, insira seu nome',
-        fill_email: 'Por favor, insira seu melhor email',
-        fill_message: 'Por favor, insira sua mensagem',
+        fill_name: "Por favor, insira seu nome",
+        fill_email: "Por favor, insira seu melhor email",
+        fill_message: "Por favor, insira sua mensagem",
 
         messages: {
           error_send_email:
-            'Erro ao enviar email, por favor tente novamente mais tarde ou entre em contato comigo pelas minhas redes sociais.',
+            "Erro ao enviar email, por favor tente novamente mais tarde ou entre em contato comigo pelas minhas redes sociais.",
           success_send_email:
-            'Email enviado com sucesso! Obrigado por entrar em contato.',
+            "Email enviado com sucesso! Obrigado por entrar em contato.",
           block_resend_email:
-            'Obrigado por entrar em contato, você já enviou um email hoje e não pode enviar outro. Entre em contato comigo pelas minhas redes sociais.',
-          thank_you_for_contacting_me: 'Obrigado por entrar em contato comigo.',
+            "Obrigado por entrar em contato, você já enviou um email hoje e não pode enviar outro. Entre em contato comigo pelas minhas redes sociais.",
+          thank_you_for_contacting_me: "Obrigado por entrar em contato comigo.",
         },
       },
     },
@@ -87,8 +83,8 @@ export default {
     // Accesibility
 
     alt_image: {
-      flag_brazil: 'Bandeira do Brasil',
-      flag_united_states: 'Bandeira dos Estados Unidos',
+      flag_brazil: "Bandeira do Brasil",
+      flag_united_states: "Bandeira dos Estados Unidos",
     },
   },
 };
