50794af870434e0efe810e7f86c1eb0a0b4fa993
felipe <profissionalf.andre@gmail.com>
2024-10-10 11:28:24 -0300

    chore: add vite plugin remove console.log to production

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import removeConsole from "vite-plugin-remove-console";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeConsole()],
   test: {
     globals: true,
     environment: "jsdom",
