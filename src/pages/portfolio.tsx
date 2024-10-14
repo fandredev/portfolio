cc2ce723aebd080152d12d7fa2c1c1d3219d4e2f
felipe <profissionalf.andre@gmail.com>
2024-10-14 17:44:17 -0300

    feat: Add react-router-dom to dependencies

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,14 +1,14 @@
 import { ToastContainer } from "react-toastify";
-import About from "./components/about";
-import Contact from "./components/contact";
-import Footer from "./components/footer";
-import Header from "./components/header";
-import Home from "./components/home";
-import Qualification from "./components/qualification";
-import ScrollUp from "./components/scrollup";
-import Skills from "./components/skills";
-import Work from "./components/work";
-import { NameProvider } from "./context/NameContext";
+import About from "../components/about";
+import Contact from "../components/contact";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import Home from "../components/home";
+import Qualification from "../components/qualification";
+import ScrollUp from "../components/scrollup";
+import Skills from "../components/skills";
+import Work from "../components/work";
+import { NameProvider } from "../context/NameContext";
 
 export default function Portfolio() {
   return (
--- src/pages/stats.tsx
+++ src/pages/stats.tsx
@@ -1,25 +1,25 @@
-import { Chart } from "react-google-charts";
-// import { useWakatimeStats } from "../hooks/use-wakatime-stats";
+// import { Chart } from "react-google-charts";
+// // import { useWakatimeStats } from "../hooks/use-wakatime-stats";
 
-export default function Stats() {
-  // const { languages } = useWakatimeStats();
+// export default function Stats() {
+//   // const { languages } = useWakatimeStats();
 
-  const data = [
-    ["Task", "Hours per Day"],
-    ["Work", 9],
-    ["Eat", 2],
-    ["Commute", 2],
-    ["Watch TV", 2],
-    ["Sleep", 7],
-  ];
+//   const data = [
+//     ["Task", "Hours per Day"],
+//     ["Work", 9],
+//     ["Eat", 2],
+//     ["Commute", 2],
+//     ["Watch TV", 2],
+//     ["Sleep", 7],
+//   ];
 
-  return (
-    <Chart
-      chartType="PieChart"
-      data={data}
-      options={{ title: "top languages" }}
-      width={"100%"}
-      height={"400px"}
-    />
-  );
-}
+//   return (
+//     <Chart
+//       chartType="PieChart"
+//       data={data}
+//       options={{ title: "top languages" }}
+//       width={"100%"}
+//       height={"400px"}
+//     />
+//   );
+// }
