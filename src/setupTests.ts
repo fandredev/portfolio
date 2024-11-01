1e299b0ee5cf92183ba149a9cc77d81b033f4ce5
felipe <profissionalf.andre@gmail.com>
2024-10-31 21:06:03 -0300

    chore: back ci again always change the language to pt-br

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,12 @@
 // import "@testing-library/jest-dom";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
+import i18n from "./i18n";
 
 afterEach(() => {
   cleanup();
+});
+
+beforeAll(() => {
+  i18n.changeLanguage("pt-BR");
 });
