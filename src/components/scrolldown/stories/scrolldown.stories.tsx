3b6d0c94fb6252489db2945c9999fa31f70edaf3
felipe <profissionalf.andre@gmail.com>
2024-11-01 01:40:33 -0300

    docs: adding scrolldown stories

---
+++ src/components/scrolldown/stories/scrolldown.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import ScrollDown from "..";
+
+const meta = {
+  component: ScrollDown,
+  tags: ["autodocs"],
+  title: "Components/ScrollDown",
+  parameters: {
+    docs: {
+      description: {
+        component:
+          "Esse componente é responsável por mostrar um ícone para o usuário descer a página para informações gerais sobre o Felipe André",
+      },
+    },
+  },
+} satisfies Meta<typeof ScrollDown>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const ScrollDownComponent: Story = {};
